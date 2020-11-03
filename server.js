b17ad6aeff628419d5a56e2cde3e0e09f1fc8327
Saynka <sankakale@gmail.com>
2020-11-02 18:40:38 -0800

    herku

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 'use strict';
 
+// Environment 
+
+
+// Application dependencies
+
 const express = require('express');
-
 const cors = require('cors');
-
+const superagent = require('superagent');
 require('dotenv').config();
-
 const PORT = process.env.PORT || 3000;
+// const { json } = require('express');
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.on('error', err => console.error(err));
 
 const app = express();
 
+//Application listen
+
+
 app.use(cors());
 
-app.use(express.static('./public'));
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // const pg = require('pg');
 
-// const client = new pg.Client(process.env.DATABASE_URL);
-
-const superagent = require('superagent');
-const { json } = require('express');
-
 app.get('/', (request, response) => {
-  response.send('Hello Sams World');
+  response.status(200).render('pages/index');
 });
+
+// app.get('/', (request, response) => {
+//   response.send('Hello Sams World');
+// });
 
 
 app.listen(PORT, () => {
