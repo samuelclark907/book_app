a8df3f71ae19fd43b1751a082b43eb12acb00715
sam <samuelclark907@gmail.com>
2020-11-07 15:34:00 -0900

    feature 1 done

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const superagent = require('superagent');
 
 require('dotenv').config();
 
-const { json } = require('express');
+const { json, response } = require('express');
 
 
 const pg = require('pg');
@@ -41,8 +41,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+// app.get('/', (request, response) => {
+//   response.status(200).render('pages/searches/new');
+// });
+
 app.get('/', (request, response) => {
-  response.status(200).render('pages/searches/new');
+  const SQL = 'SELECT * FROM books';
+
+  client.query(SQL)
+    .then(results => {
+      let bookInfo = results.rows;
+      // let booksLength = results.rows.length;
+      // console.log(bookInfo);
+      response.status(200).render('pages/index', { bikes: bookInfo });
+    });
 });
 
 client.connect()
@@ -52,17 +64,23 @@ client.connect()
     });
   });
 
-app.get('/error', errorHandler);
-
 app.get('/searches/new', (request, response) => {
   response.status(200).render('pages/searches/new');
 });
+app.post('/add', addBooks);
 
 app.post('/searches', bookHandler);
+app.get('*', (request, response) => response.status(404).send('This route not here'));
+
+app.get((error, request, response) => {
+  return errorHandler(error, response);
+});
+
+app.get('/books/:id', detailBook);
 
 // functions
 
-function bookHandler(request,response) {
+function bookHandler(request, response) {
   const search = request.body.search;
   const searchField = request.body.searchField;
   let useField = '';
@@ -85,15 +103,43 @@ function bookHandler(request,response) {
         } else { image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQjim8QJc5Eup07gmPSuP2gStB8jZauU8kK0A&usqp=CAU'; }
         return new Book(book, image);
       });
-      response.status(200).render('pages/searches/show', { bikes });
+      response.status(200).render('pages/searches/show', { bikes: bikes });
     });
 }
 
 
 
+function addBooks(request, response) {
+  const SQL = 'INSERT INTO books (image, title, author, description) VALUES ($1, $2, $3, $4);';
+  const params = [request.body.image, request.body.title, request.body.author, request.body.description];
+  // console.log('req', request.body);
+  client.query(SQL, params)
+    .then(results => {
+      // console.log(results.rows);
+      response.status(200).redirect('/');
+    });
+}
+
+function detailBook(request, response) {
+  console.log('I');
+  const SQL = 'SELECT * FROM books WHERE id=$1;';
+  const params = [request.params.id];
+  console.log(params);
+
+  client.query(SQL, params)
+    .then(results => {
+      console.log(results.rows);
+      response.status(200).render('pages/books/details');
+    });
+}
+
+
+
+
+
 // Error Handler///////////////////
-function errorHandler(request, response) {
-  response.status(500).render('pages/error');
+function errorHandler(error, response) {
+  response.status(500).render('pages/error', { error: error });
 }
 
 // Constructors///////////////////
